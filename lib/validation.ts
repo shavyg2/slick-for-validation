e0848fcb05a2dfca1fee0d64972250f4378f0864
Shavauhn Gabay <shavyg2@gmail.com>
2020-02-18 02:14:47 -0500

    first push

---
+++ lib/validation.ts
@@ -0,0 +1,87 @@
+
+import * as types from "utility-types";
+import * as traverse from "traverse";
+import is from "@sindresorhus/is";
+
+type NotPromise<T,S=any> = T extends Promise<infer S>?S:T
+type OnlyMethods<T> = {[K in keyof T]:T[K] extends (value:any,err:string[])=>any?NotPromise<ReturnType<T[K]>>:OnlyMethods<T[K]>}; 
+
+type ToType<T,S=any> = {[K in keyof T]:T[K] extends (value:infer S,error:string[])=>void|Promise<void>?NotPromise<S>:ToType<T[K],S>}
+
+export function Validation<T>(value:ToType<T>,validation:T):Promise<[string[],ToType<T>]>|[string[],ToType<T>]{
+    const test = traverse.default(value)
+    const errorList:string[] = []
+
+    const results = traverse.reduce(validation,function(acc,func:(property,error:string[])=>void){
+        if(this.isLeaf && typeof func === "function"){
+            const self = this;
+            if(is.promise(acc)){
+
+                return acc.then(function(){
+                    const path = self.path;
+                    const parent= (self.parent||{}).node;
+                    const testable = test.get(path);
+                    const result = func.call(parent,testable,errorList)
+                    return result;
+                })
+
+            }else{
+                const path = self.path;
+                const parent= (self.parent||{}).node;
+                const testable = test.get(path);
+                const result = func.call(parent,testable,errorList)
+                return result;
+
+            }
+
+        }else if(this.isLeaf){
+            throw new Error("Validation Object must contain only functions")
+        }
+    },null as any)
+
+    if(is.promise(results)){
+        return results.then(()=>{
+            return [errorList.length>0?errorList:null as any as string[],value]
+        })
+    }else{
+        return [errorList.length>0?errorList:null as any as string[],value];
+    }
+}
+
+
+export function ValidationSync<T>(value:ToType<T>,validation:T):[string[],ToType<T>]{
+    const result = Validation(value,validation)
+    if(is.promise(result)){
+        throw new Error("Validation sync can't have async methods")
+    }else{
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/validation.spec.ts
@@ -0,0 +1,39 @@
+import is from "@sindresorhus/is/dist"
+import { ValidationSync } from "../lib/validation"
+
+
+
+
+describe("Sync Validation",()=>{
+
+
+    const validation = {
+        name(name:string,error:string[]){
+            if(!is.string(name)){
+                error.push("property name is required")
+            }
+        }
+    }
+
+
+    it("should be able to validate",()=>{
+        const [err,person] = ValidationSync({
+            name:"Sarah"
+        },validation)
+
+        expect(err).toBeFalsy();
+        expect(person.name).toBe("Sarah");
+
+    })
+
+
+    it("should not validate incorrect person",()=>{
+        const raw = {} as any;
+        const [err,person] =  ValidationSync(raw,validation);
+        expect(err).toBeTruthy();
+        expect(err.length).toBe(1);
+        expect(raw).toBe(person);
+    })
+
+
+})
